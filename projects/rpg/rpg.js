7221da0c20a8ab6845f9ea81aeeb862bb9aeb19f
Gabe Hare <gabehare90@gmail.com>
2017-01-18 13:23:10 -0700

    Updated gitignore and project files

--- projects/rpg/rpg.js
+++ projects/rpg/rpg.js
@@ -2,24 +2,26 @@ var readline = require("readline-sync");
 var name = readline.question("What is your name? ");
 console.log(name);
 console.log("Hey " + name + " let's go on an adventure together! Grab your torch and pichfork. We are going hunting!\n Received torch \n Received pitchfork")
-console.log("Press w to begin walking");
 
 function getRandomInt(max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var key = readlineSync.keyIn("w");
+var walking = readline.keyIn("To fight, press y, to run press n ", {limit:'yn'});
 
 function walk() {
     var w = true
-    while (w === true) { 
-        getRandomInt (4, 1);
-        if (getRandomInt === 1){
+    while (w === true) {
+        getRandomInt(4, 1);
+        if (getRandomInt === 1) {
             w === false;
-            
+           return("You are being attacked by oompa-loompa");
+
         }
-    } 
-    
-})
+
+    }
+}
+walk();
+
 
 
 
